8393af7e5e0a5209c0136b3867a950c1cfb70d83
marcioszka <monkiewicz.marta@gmail.com>
2023-05-28 19:01:11 +0200

    Add image path property

--- src/Codecool.CodecoolShop/Models/Product.cs
+++ src/Codecool.CodecoolShop/Models/Product.cs
@@ -9,6 +9,8 @@ namespace Codecool.CodecoolShop.Models
         public ProductCategory ProductCategory { get; set; }
         public Supplier Supplier { get; set; }
 
+        public string ImagePath { get; set; }
+
         public void SetProductCategory(ProductCategory productCategory)
         {
             ProductCategory = productCategory;
