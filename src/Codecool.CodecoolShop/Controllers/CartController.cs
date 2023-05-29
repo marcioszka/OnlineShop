9d94c7c9540b6aac0728f87deb6e811c7adec1bb
marcioszka <monkiewicz.marta@gmail.com>
2023-05-29 18:21:56 +0200

    Add cart controller

---
+++ src/Codecool.CodecoolShop/Controllers/CartController.cs
@@ -0,0 +1,25 @@
+﻿using Codecool.CodecoolShop.Daos.Implementations;
+using Codecool.CodecoolShop.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Codecool.CodecoolShop.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly ILogger<CartController> _logger;
+        //public CartService CartService { get; set; }
+
+        public CartController(ILogger<CartController> logger)
+        {
+            _logger = logger;
+            //CartService = new CartService(
+            //    ProductDaoDB.GetInstance();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
