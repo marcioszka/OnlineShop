d83f687e437e8991ca49f643177e4eec8dcd612d
marcioszka <monkiewicz.marta@gmail.com>
2023-05-29 18:39:23 +0200

    Add service for cart

---
+++ src/Codecool.CodecoolShop/Services/CartService.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Codecool.CodecoolShop.Daos;
+using Codecool.CodecoolShop.Models;
+
+namespace Codecool.CodecoolShop.Services
+{
+    public class CartService
+    {
+        //private readonly IOrderDao orderDaoDB;
+        //private readonly ILineItemDao lineItemDao;
+        
+        //public CartService(IOrderDao orderDaoDB, ILineItemDao lineItemDao)
+        //{
+        //    this.orderDaoDB = orderDaoDB;
+        //    this.lineItemDao = lineItemDao;
+        //}
+
+        //public void SaveOrder() => this.orderDaoDB.Add();
+
+        //public Order GetOrderById(int orderId) => this.orderDaoDB.Get(orderId);
+        
+
+        //public IEnumerable<Order> GetAllOrders() => this.orderDaoDB.GetAll();
+    }
+}
