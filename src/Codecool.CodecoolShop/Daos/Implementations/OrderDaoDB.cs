8b656d77d2bb57db8e42ed1d331e8c087ac41288
marcioszka <monkiewicz.marta@gmail.com>
2023-05-29 22:40:19 +0200

    Write methods to implement order and its details in DB

--- src/Codecool.CodecoolShop/Daos/Implementations/OrderDaoDB.cs
+++ src/Codecool.CodecoolShop/Daos/Implementations/OrderDaoDB.cs
@@ -1,6 +1,9 @@
 ﻿using Codecool.CodecoolShop.Models;
+using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
 
 namespace Codecool.CodecoolShop.Daos.Implementations
 {
@@ -29,9 +32,29 @@ namespace Codecool.CodecoolShop.Daos.Implementations
 
         public void Add(Order item)
         {
-            throw new System.NotImplementedException();
-        }
+            try
+            {
+                using var connection = new SqlConnection(_connectionString);
+                connection.Open();
+                using var command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
 
+                string insertOrderSql =
+                    @"
+                    INSERT INTO orders (id, userID)
+                    VALUES (@Id, @UserID);
+                    ";
+
+                command.CommandText = insertOrderSql;
+                command.Parameters.AddWithValue("@Id", item.Id);
+                command.Parameters.AddWithValue("@UserID", 1);
+            }
+            catch (SqlException exception)
+            {
+                throw exception;
+            }
+        }
+        
         public void Remove(int id)
         {
             throw new System.NotImplementedException();
@@ -45,6 +68,79 @@ namespace Codecool.CodecoolShop.Daos.Implementations
         public IEnumerable<Order> GetAll()
         {
             throw new System.NotImplementedException();
+        }
+
+        public void AddOrderDetails(Order order)
+        {
+            try
+            {
+                using var connection = new SqlConnection(_connectionString);
+                connection.Open();
+                using var command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
+
+                foreach(var item in order.Items)
+                {
+                    string insertOrderDetailsSql =
+                    @"
+                    INSERT INTO line_item (id, name, price, quantity, order_id)
+                    VALUES (@Id, @Name, @Price, @Quantity, @Order_id);
+                    ";
+
+                    command.CommandText = insertOrderDetailsSql;
+                    command.Parameters.AddWithValue("@Id", item.Id);
+                    command.Parameters.AddWithValue("@Name", item.Name);
+                    command.Parameters.AddWithValue("@Price", item.Price);
+                    command.Parameters.AddWithValue("@Quantity", item.Quantity);
+                    command.Parameters.AddWithValue("@Order_id", order.Id);
+                }
+            }
+            catch (SqlException exception)
+            {
+                throw exception;
+            }
+        }
+
+        public List<LineItem> GetOrderDetails(int orderId)
+        {
+            try
+            {
+                using var connection = new SqlConnection(_connectionString);
+                connection.Open();
+                using var command = connection.CreateCommand();
+                command.CommandType = CommandType.Text;
+
+                string selectOrderDetailsSql =
+                    @"
+                    SELECT id, name, price, quantity
+                    FROM line_item
+                    WHERE order_id=@Order_id;
+                    ";
+
+                command.CommandText = selectOrderDetailsSql;
+                command.Parameters.AddWithValue("@Order_id", orderId);
+
+                using var reader = command.ExecuteReader();
+                List<LineItem> data = new List<LineItem>();
+
+                while (reader.Read())
+                {
+                    int id = (int)reader["id"];
+                    string name = (string)reader["name"];
+                    decimal price = (decimal)reader["price"];
+                    int quantity = (int)reader["quantity"];
+
+                    LineItem item = new LineItem(name, price, quantity) { Id = id };
+
+                    data.Add(item);
+                }
+
+                return data;
+            }
+            catch (SqlException exception)
+            {
+                throw exception;
+            }
         }
     }
 }
