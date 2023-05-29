1bf8eee1e1f61394159f1ec3ec1de0ada77845df
marcioszka <monkiewicz.marta@gmail.com>
2023-05-29 22:12:14 +0200

    Add methods for OrderDao

--- src/Codecool.CodecoolShop/Daos/IOrderDao.cs
+++ src/Codecool.CodecoolShop/Daos/IOrderDao.cs
@@ -1,9 +1,12 @@
 ﻿using Codecool.CodecoolShop.Models;
+using System.Collections.Generic;
 
 namespace Codecool.CodecoolShop.Daos
 {
     public interface IOrderDao : IDao<Order>
     {
+        public void AddOrderDetails(Order item);
 
+        public List<LineItem> GetOrderDetails(Order item);
     }
 }
