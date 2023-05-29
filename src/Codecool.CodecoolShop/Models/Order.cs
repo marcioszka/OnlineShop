6f4e7c897aa830fae09cf2ccbd6eec0959371994
marcioszka <monkiewicz.marta@gmail.com>
2023-05-29 17:53:04 +0200

    Add new class Order

---
+++ src/Codecool.CodecoolShop/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Codecool.CodecoolShop.Models
+{
+    public class Order
+    {
+        private static int idCounter = 0;
+        public int Id { get; set; }
+        public List<LineItem> Items { get; set; }
+
+        public Order(string name, decimal price) 
+        {
+            this.Id = System.Threading.Interlocked.Increment(ref idCounter);
+            this.Items = new List<LineItem>{ new LineItem(name, price)  };
+        }
+    }
+}
