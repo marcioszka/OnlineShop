b40f24115b75117e36dc4bb10de0b30aca6b6fd4
marcioszka <monkiewicz.marta@gmail.com>
2023-05-29 17:51:27 +0200

    Add autoincrement of id

--- src/Codecool.CodecoolShop/Models/LineItem.cs
+++ src/Codecool.CodecoolShop/Models/LineItem.cs
@@ -2,14 +2,15 @@
 {
     public class LineItem
     {
+        private static int idCounter = 0;
         public int Id { get; set; }
         public string Name { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
 
-        public LineItem(string name, decimal price, int quantity=1, int id=1) 
+        public LineItem(string name, decimal price, int quantity=1) 
         {
-            Id = id;
+            this.Id = System.Threading.Interlocked.Increment(ref idCounter);
             Name = name;
             Price = price;
             Quantity = quantity;
