b6444449d0b434d7348c2d4bf8c330a94c718aa2
marcioszka <monkiewicz.marta@gmail.com>
2023-05-29 17:38:50 +0200

    Define new class for shopping

---
+++ src/Codecool.CodecoolShop/Models/LineItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Codecool.CodecoolShop.Models
+{
+    public class LineItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+
+        public LineItem(string name, decimal price, int quantity=1, int id=1) 
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+    }
+}
