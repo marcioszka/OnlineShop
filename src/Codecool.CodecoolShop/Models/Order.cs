584f0111106bb38332acf18c330c2525bab7f282
marcioszka <monkiewicz.marta@gmail.com>
2023-05-30 12:03:27 +0200

    Add get instance and add line item methods

--- src/Codecool.CodecoolShop/Models/Order.cs
+++ src/Codecool.CodecoolShop/Models/Order.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using Codecool.CodecoolShop.Daos.Implementations;
+using System.Collections.Generic;
 
 namespace Codecool.CodecoolShop.Models
 {
@@ -7,11 +8,27 @@ namespace Codecool.CodecoolShop.Models
         private static int idCounter = 0;
         public int Id { get; set; }
         public List<LineItem> Items { get; set; }
+
+        private static Order instance = null;
+
         public Order(string name, decimal price) 
         {
             this.Id = System.Threading.Interlocked.Increment(ref idCounter);
             this.Items = new List<LineItem>{ new LineItem(name, price)  };
         }
+       
+        public static Order GetInstance(string name, decimal price)
+        {
+            if (instance == null)
+            {
+                instance = new Order(name, price);
+            }
+
+            return instance;
+        }
+
+        public void AddLineItem(string name, decimal price) => this.Items.Add(new LineItem(name, price));
+
 
         public override string ToString()
         {
