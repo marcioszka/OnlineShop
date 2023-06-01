9626c709f18ccfa3f34ea118b1479111135242c7
marcioszka <monkiewicz.marta@gmail.com>
2023-06-01 14:18:31 +0200

    Add currency property to LineItem

--- src/Codecool.CodecoolShop/Models/LineItem.cs
+++ src/Codecool.CodecoolShop/Models/LineItem.cs
@@ -4,23 +4,16 @@ namespace Codecool.CodecoolShop.Models
 {
     public class LineItem
     {
-        private static int idCounter;
         public int Id { get; set; }
         public string Name { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
 
+        public string Currency { get; set; }
+
         public LineItem(int id)
         {
             Id = id;
         }
-
-        //public LineItem(int id, string name, decimal price, int quantity = 1)
-        //{
-        //    Id = id;
-        //    Name = name;
-        //    Price = price;
-        //    Quantity = quantity;
-        //}
     }
 }
